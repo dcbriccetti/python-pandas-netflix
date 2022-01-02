005c6500653901eabbae6b8fa38c4a194f8e1d0c
Dave Briccetti <daveb@davebsoft.com>
2022-01-01 16:41:45 -0800

    Improve readability

--- viewing_activity.py
+++ viewing_activity.py
@@ -1,25 +1,32 @@
 'Analyze Netflix Viewing Activity (https://help.netflix.com/en/node/101917)'
 
 import pandas as pd
+from pandas import DataFrame
 from colorama import Fore
 
-report_dir = '/Users/daveb/Documents/netflix-report/'
-title_search = 'Seinfeld'
+REPORT_DIR = '/Users/daveb/Documents/netflix-report/'
 
-svt = 'Supplemental Video Type'
-df = pd.read_csv(
-    report_dir + 'CONTENT_INTERACTION/ViewingActivity.csv',
-    index_col='Title',
-    usecols=['Title', 'Start Time', 'Duration', svt],
-    parse_dates=['Start Time']) \
-    .fillna('') \
-    .rename(columns={svt: 'Type'})
-df['Duration'] = pd.to_timedelta(df['Duration'])
+def config_display() -> None:
+    'Show more rows and columns than the default'
+    for part in 'max_columns, min_rows, max_rows, width, max_colwidth'.split(', '):
+        pd.set_option('display.' + part, None)
+    pd.set_option('display.max_rows', 35)
 
-title_matches = df.index.str.contains(title_search)
-sdf = df[title_matches].sort_values(by='Start Time')
-for part in 'max_columns, min_rows, max_rows, width, max_colwidth'.split(', '):
-    pd.set_option('display.' + part, None)
-pd.set_option('display.max_rows', 35)
-print(sdf, '\n')
-print(Fore.LIGHTYELLOW_EX + 'Total watch time:', sdf['Duration'].sum())
+def get_title_matches(title_part: str, df: DataFrame) -> DataFrame:
+    return df[df.index.str.contains(title_part)]
+
+def create_dataframe():
+    svt = 'Supplemental Video Type'
+    df: DataFrame = pd.read_csv(
+        REPORT_DIR + 'CONTENT_INTERACTION/ViewingActivity.csv', index_col='Title',
+        usecols=['Title', 'Start Time', 'Duration', svt], parse_dates=['Start Time']) \
+        .fillna('') \
+        .rename(columns={svt: 'Type'})
+    df['Duration'] = pd.to_timedelta(df['Duration'])
+    return df.sort_values(by='Start Time')
+
+config_display()
+df = create_dataframe()
+mdf = get_title_matches('Seinfeld', df)
+print(mdf, '\n')
+print(Fore.LIGHTYELLOW_EX + 'Total watch time:', mdf['Duration'].sum())
